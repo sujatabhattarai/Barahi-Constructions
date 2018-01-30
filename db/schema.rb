610377eb63260f3f898a4a44f169cff7fd3c0131
Sujata <bhattaraisuzata@gmail.com>
2018-01-29 21:57:26 -0600

    deleted users table, model, comtroller and views

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180125202613) do
+ActiveRecord::Schema.define(version: 20180129190109) do
 
   create_table "employees", force: :cascade do |t|
     t.string "first_name"
@@ -24,5 +24,4 @@ ActiveRecord::Schema.define(version: 20180125202613) do
     t.string "name"
     t.integer "count"
   end
-
 end
